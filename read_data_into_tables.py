e2937f63784f7da2e2b421f9168342b71e0d460d
Hussein Darman <husseinmdarman@gmail.com>
2025-12-27 13:16:08 +0000

    made one function which reads csv into tables currently working with both airport and airline data

--- read_data_into_tables.py
+++ read_data_into_tables.py
@@ -1,13 +1,17 @@
 from database_connection_utils import create_engine_from_creds
 from create_classes_for_tables import Airline, Airport
+from sqlalchemy.orm import DeclarativeMeta
+from typing import Iterable, Optional, Type
 import pandas as pd
 import os
 
-def read_airline_data_into_table(csv_file_path):
+def read_data_into_table(csv_file_path: str, Table_to_be_loaded: Type[DeclarativeMeta] , desired_columns: Optional[Iterable[str]] = None ):
     """
-    Reads airline data from a CSV file and inserts it into the Airline table in the database.
+    Reads data from a CSV file and inserts it into the table in the database.
     Args:
         csv_file_path (str): Path to the CSV file containing airline data.
+        Table_to_be_loaded (Type[DeclarativeMeta]): The SQLAlchemy ORM class representing the table to load data into.
+        desired_columns (Optional[Iterable[str]]): List of columns to read from the CSV. If None, all columns are read.
     Returns:
         None        
     """
@@ -17,34 +21,19 @@ def read_airline_data_into_table(csv_file_path):
     engine = create_engine_from_creds() # Create database engine
    
     # Read CSV data into a DataFrame
-    airline_df = pd.read_csv(csv_file_path)
-
-    # Insert data into the Airline table
-    airline_df.to_sql(Airline.__tablename__, engine, if_exists='append', index=False)
-    print(f"Inserted {len(airline_df)} records into the Airline table.")
-
-def read_airport_data_into_table(csv_file_path):
-    """
-    Reads airline data from a CSV file and inserts it into the Airline table in the database.
-    Args:
-        csv_file_path (str): Path to the CSV file containing airline data.
-    Returns:
-        None        
-    """
-    if not os.path.exists(csv_file_path):
-         raise FileNotFoundError(f"File not found: {csv_file_path}") # Check if the file exists
     
-    engine = create_engine_from_creds() # Create database engine
-    # Desired order of columns in the Airport table
-    desired_ordered_columns = ["IATA Code", "Airport Name", "City", "Country", "Region"]  
-    # Read CSV data into a DataFrame
-    airport_df = pd.read_csv(csv_file_path, usecols=desired_ordered_columns)
+    table_to_be_loaded_df = pd.read_csv(csv_file_path, usecols=desired_columns or None)
+    
 
     # Insert data into the Airline table
-    airport_df.to_sql(Airport.__tablename__, engine, if_exists='append', index=False)
-    print(f"Inserted {len(airport_df)} records into the Airport table.")
+    table_to_be_loaded_df.to_sql(Table_to_be_loaded.__tablename__, engine, if_exists='replace', index=False)
+    print(f"Inserted {len(table_to_be_loaded_df)} records into the {Table_to_be_loaded.__tablename__} table.")
+
 
 if __name__ == "__main__":
 
-    read_airline_data_into_table("Data/airline.csv")
-    read_airport_data_into_table("Data/airports.csv")
+    read_data_into_table("Data/airline.csv", Airline)
+
+    desired_ordered_columns = ["IATA Code", "Airport Name", "City", "Country", "Region"] 
+    read_data_into_table("Data/airports.csv", Airport, desired_columns=desired_ordered_columns)
+    
