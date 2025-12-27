0aea37bfb4a6a1da7bbb4f2e72b0e7fa552fcc34
Hussein Darman <husseinmdarman@gmail.com>
2025-12-27 18:13:17 +0000

    have it fully functional between the three tables countryRegion, airline and airport

--- read_data_into_tables.py
+++ read_data_into_tables.py
@@ -10,13 +10,13 @@ Base.metadata.create_all(engine)
 
 def read_csv_data_into_dataframe(csv_file_path: str, desired_columns: Optional[Iterable[str]] = None ):
     """
-    Reads data from a CSV file and inserts it into the table in the database.
+    Reads CSV data into a pandas DataFrame.
     Args:
-        csv_file_path (str): Path to the CSV file containing airline data.
-        Table_to_be_loaded (Type[DeclarativeMeta]): The SQLAlchemy ORM class representing the table to load data into.
+        csv_file_path (str): The path to the CSV file.  
         desired_columns (Optional[Iterable[str]]): List of columns to read from the CSV. If None, all columns are read.
-    Returns:
-        None        
+        Returns:
+        pd.DataFrame: The loaded DataFrame.
+    
     """
     if not os.path.exists(csv_file_path):
          raise FileNotFoundError(f"File not found: {csv_file_path}") # Check if the file exists
@@ -37,31 +37,14 @@ def load_df_sql(dataframe_to_upload: pd.DataFrame, Table_to_be_loaded: Type[Decl
     Returns:
         None        
     """
+    #Primary Key column name
+    primary_key_name = Table_to_be_loaded.__table__.primary_key.columns[0].name
+    
+    existing = pd.read_sql(f'SELECT "{primary_key_name}" FROM "{Table_to_be_loaded.__tablename__}"', engine)
+    dataframe_to_upload = dataframe_to_upload[~dataframe_to_upload[primary_key_name].isin(existing[primary_key_name])]
     # Insert data into the specified table
     dataframe_to_upload.to_sql(Table_to_be_loaded.__tablename__, engine, if_exists='append', index=False)
     print(f"Inserted {len(dataframe_to_upload)} records into the {Table_to_be_loaded.__tablename__} table.")
-
-def read_csv_data_into_table(csv_file_path: str, Table_to_be_loaded: Type[DeclarativeMeta] , desired_columns: Optional[Iterable[str]] = None ):
-    """
-    Reads data from a CSV file and inserts it into the table in the database.
-    Args:
-        csv_file_path (str): Path to the CSV file containing airline data.
-        Table_to_be_loaded (Type[DeclarativeMeta]): The SQLAlchemy ORM class representing the table to load data into.
-        desired_columns (Optional[Iterable[str]]): List of columns to read from the CSV. If None, all columns are read.
-    Returns:
-        None        
-    """
-    if not os.path.exists(csv_file_path):
-         raise FileNotFoundError(f"File not found: {csv_file_path}") # Check if the file exists
-   
-    # Read CSV data into a DataFrame
-    
-    table_to_be_loaded_df = pd.read_csv(csv_file_path, usecols=desired_columns or None)
-    
-
-    # Insert data into the Airline table
-    table_to_be_loaded_df.to_sql(Table_to_be_loaded.__tablename__, engine, if_exists='replace', index=False)
-    print(f"Inserted {len(table_to_be_loaded_df)} records into the {Table_to_be_loaded.__tablename__} table.")
 
 def create_countryregion_table(airline_csv_file_path: str,airport_csv_file_path: str,Table_to_be_loaded: Type[DeclarativeMeta]):
     
@@ -104,27 +87,8 @@ def create_countryregion_table(airline_csv_file_path: str,airport_csv_file_path:
 if __name__ == "__main__":
    
    airports_df, airlines_df= create_countryregion_table("Data/airline.csv","Data/airports.csv", CountryRegion)
-   print(airports_df.head(10))
-   print(airlines_df.head(10))
-#    load_df_sql(airports_df, Airport)
-#    load_df_sql(airlines_df, Airline)
+   airports_df.rename(columns={"Airport Name": "Airport_name", "IATA Code": "IATA"}, inplace=True)
+   print(airports_df.head(10)) 
+   load_df_sql(airports_df, Airport)
+   load_df_sql(airlines_df, Airline)
    
-
-    # airlines_df = pd.read_csv("Data/airline.csv") 
-    # airports_df = pd.read_csv("Data/airports.csv")
-    
-    # #Extract unique combinations of country and region from dataframe
-    # country_region_df = ( 
-    #     pd.concat([ 
-    #         airlines_df[['Country', 'Region']], 
-    #         airports_df[['Country', 'Region']] ])
-    #         .drop_duplicates()
-    #         .reset_index(drop=True) )
-
-    # print(country_region_df.head(10))
-    
-    # read_data_into_table("Data/airline.csv", Airline)
-
-    # desired_ordered_columns = ["IATA Code", "Airport Name", "City", "Country", "Region"] 
-    # read_data_into_table("Data/airports.csv", Airport, desired_columns=desired_ordered_columns)
-    
